15f32b59d7e0a4b8b1146f006036eedd5b108ace
PhilipNzube <72675495+PhilipNzube@users.noreply.github.com>
2024-07-08 09:43:48 +0100

    Add files via upload

--- src/Pages/TicketOne.js
+++ src/Pages/TicketOne.js
@@ -508,8 +508,10 @@ export default function TicketOne() {
                             <div className="fade-in">
                                 <div className="phone-container">
                                     <div className="phone-frame" id="phoneFrame">
+                                        <div className="notch" id="notch"></div>
                                         <iframe src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com/proto/1ULaycsRlcuQlUex1CnM70/BillMe-Mobile-App-Highfi?page-id=0%3A1&node-id=1-506&viewport=483%2C377%2C0.13&t=sEelkaoSAFlX7iDc-1&scaling=scale-down&starting-point-node-id=1%3A135" allowFullScreen></iframe>
                                     </div>
+                                    {/* <label id="phoneLabel">iPhone</label> */}
                                 </div>
                             </div>
                             {/* <img id="TicketOnePrototypeImg" src={TicketOnePrototypeImg} alt="TicketOnePrototypeImg" /> */}
